0e42327d039081ee90f2300d69dcdc414e44f6b6
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2022-10-29 16:53:37 +0530

    Added hashing

--- server/database/user/index.js
+++ server/database/user/index.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -14,10 +16,49 @@ const UserSchema = new mongoose.Schema(
 );
 
 //attachments
-UserSchema.methods.generateJwtTokens = function () {};
+UserSchema.methods.generateJwtTokens = function () {
+  return jwt.sign({ user: this._id.toString() }, "akash");
+};
 
 //helpers functions
-UserSchema.statics.findByEmailandPhone = async () => {};
-UserSchema.statics.findByEmailandPassword = async () => {};
+UserSchema.statics.findByEmailandPhone = async ({ email, contact }) => {
+  const checkemail = await UserModel.findOne({ email });
+  const checkphone = await UserModel.findOne({ contact });
 
+  if (checkemail || checkphone) {
+    throw new Error("User Already exists...!");
+  }
+  return false;
+};
+UserSchema.statics.findByEmailandPassword = async ({ email, password }) => {
+  const user = await UserModel.findOne({ email });
+  if (!user) {
+    throw new Error("User does not exists..!");
+  }
+  const checkpass = await bcrypt.compare(password, this.password);
+  if (!checkpass) {
+    throw new Error("Invalid Password...!");
+  }
+  return user;
+};
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+
+  //pass is modified
+  if (user.isModified("password")) return next();
+
+  //salting pass
+  bcrypt.genSalt(2, (error, salt) => {
+    if (error) return next(error);
+
+    //hashing
+    bcrypt.hash(user.password, salt, (error, hash) => {
+      if (error) return next(error);
+
+      user.password = hash;
+      return next();
+    });
+  });
+});
 export const UserModel = mongoose.model("users", UserSchema);
